789a5b4a72258a57558aa82a79ac1d9d52a832a6
AsHim Shrestha <ashimsth89@gmail.com>
2025-12-06 18:31:23 +0545

    add if_else.go with age eligibility check

---
+++ if_else/if_else.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main () {
+
+	age := 28 
+
+	if age >= 28 {
+		fmt.Println("You are eligible for marriage")
+	} else {
+		fmt.Println("You are not eligible for marriage")
+	}
+
+}
