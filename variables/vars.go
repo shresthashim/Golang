7cd31fe417507a05af22a2ecf058e69e0dd81a31
AsHim Shrestha <ashimsth89@gmail.com>
2025-12-06 18:20:53 +0545

    restore hello_world.go and add variables example

---
+++ variables/vars.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Golang"
+
+	// infer type
+	var name = "Golang"
+
+	fmt.Println(name)
+
+	var age int = 10
+	fmt.Println(age)
+
+	// short declaration
+	country := "USA"
+	fmt.Println(country)
+
+}
